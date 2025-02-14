84181fbafe30838ebc9f2398fa8e77cc8b2fe8f1
ukuleleEric <vuarnet0318@gmail.com>
2025-02-14 16:28:13 +0800

    feat: 13 working with data

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-Vue.createApp({}).mount("#app");
+const lastName = "Doe";
+
+Vue.createApp({
+  data() {
+    return {
+      firstName: "John", //<-- 建立在createApp()內物件的data()，其回傳的key值會對應到HTML中的雙花括號的key，同時渲染出他的值。
+      lastName: "OK",
+    };
+  },
+}).mount("#app");
