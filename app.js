b2615d448651448dd1dcde12a034f2545fcf5b93
ukuleleEric <vuarnet0318@gmail.com>
2025-02-15 17:02:49 +0800

    14_access_the_instance_data

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const lastName = "Doe";
 
-Vue.createApp({
+const vm = Vue.createApp({
   data() {
     return {
       firstName: "John", //<-- 建立在createApp()內物件的data()，其回傳的key值會對應到HTML中的雙花括號的key，同時渲染出他的值。
@@ -8,3 +8,8 @@ Vue.createApp({
     };
   },
 }).mount("#app");
+
+// 設定2秒鐘之後，變更firstName的值
+setTimeout(() => {
+  vm.firstName = "Eric";
+}, 2000);
