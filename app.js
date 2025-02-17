280ab482775a3fedba939521a8977de4f6f9fcaf
ukuleleEric <vuarnet0318@gmail.com>
2025-02-17 10:38:20 +0800

    19_v-html

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const vm = Vue.createApp({
       firstName: "John", //<-- 建立在createApp()內物件的data()，其回傳的key值會對應到HTML中的雙花括號的key，同時渲染出他的值。
       lastName: "OK",
       url: "https://www.google.com",
+      raw_url: '<a href="www.google.com">Google</a>',
     };
   },
   methods: {
